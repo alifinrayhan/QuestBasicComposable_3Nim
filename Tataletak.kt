d8b6396088b47d4ec48b6ae4594040243d8912d6
alifinrayhan <160594805+alifinrayhan@users.noreply.github.com>
2025-10-16 15:02:10 +0700

    Implement TataletakBox composable

--- Tataletak.kt
+++ Tataletak.kt
@@ -45,3 +45,19 @@ fun TataletakRow(modifier: Modifier = Modifier) {
         Text(text = "Komponen4")
     }
 }
+
+@Composable
+fun TataletakBox(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Row 2")
+    }
+}
+
